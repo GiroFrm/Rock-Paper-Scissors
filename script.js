87a5c32d71cb33ab6c8c22c26c110145da3cc216
GiroFrm <girofram844@gmail.com>
2024-09-25 19:49:44 +1000

    refactor: Refactor playRound function to track and increment human and computer scores
    This function now increments the `humanScore` and `computerScore` variables
    
    based on the outcome of each round. It logs the result
    
    of the round and updates the respective scores.

--- script.js
+++ script.js
@@ -34,27 +34,42 @@ function playRound(humanChoice, computerChoice){
   
     if(humanChoice === "paper" && computerChoice === "scissors") {
 
-      console.log("You lose! Scissors beats Paper")
+      console.log("You lose! Scissors beats Paper");
+      computerScore++;
 
     } else if(humanChoice === "scissors" && computerChoice === "paper") {
 
       console.log("You win! Scissors beats Paper")
+      humanScore++;
 
     } else if(humanChoice === "paper" && computerChoice === "rock"){
 
       console.log("You win! Paper beats Rock")
+      humanScore++;
 
     } else if(humanChoice === "rock" && computerChoice === "paper"){
       
         console.log("You lose! Paper beats Rock")
+        computerScore++;
     
     } else if(humanChoice === "rock" && computerChoice === "scissors"){
 
-        console.log("You lose! scissors beats rock ")
+        console.log("You win! scissors beats rock ")
+        humanScore++;
     
     } else if(humanChoice === "scissors" && computerChoice === "rock"){
 
         console.log("You lose! scissors beats rock ")
+        computerScore++;
     } 
 
 }
+
+playRound("rock", "paper");
+playRound("rock", "scissors");
+playRound("paper", "scissors");
+playRound("scissors", "paper");
+playRound("scissors", "paper");
+
+console.log(computerScore);
+console.log(humanScore);
