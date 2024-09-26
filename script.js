ffc04362d97ca7ae2bd49d75738bbfb71ac6f5c6
GiroFrm <girofram844@gmail.com>
2024-09-26 18:18:37 +1000

    refactor: Remove conosle.log

--- script.js
+++ script.js
@@ -17,16 +17,11 @@ function getComputerChoice(){
 
 }
 
-//console.log(getComputerChoice());
-
 function getHumanChoice(){
     let choice = prompt("what's your choice?");
     
     return choice;
 }
-
-//console.log(getHumanChoice())
-
 
 
 function playGame(){
